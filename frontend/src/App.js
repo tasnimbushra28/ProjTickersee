2821eae7a2cdfacb82fcc059bca28ed99a2065f2
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-18 00:16:01 +0600

    Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,40 @@
 import data from './data';
+import { BroswerRouter, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">tickersee</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
+    <BroswerRouter>
+      <div>
+        <header>
+          <a href="/">tickersee</a>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          <h1>Featured Products</h1>
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product" key={product.slug}>
                 <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
+                  <img src={product.image} alt={product.name} />
                 </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
+                <div className="product-info">
+                  <a href={`/product/${product.slug}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+            ))}
+          </div>
+        </main>
+      </div>
+    </BroswerRouter>
   );
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,4 @@
+function HomeScreen() {
+  return <div>Home Screen</div>;
+}
+export default HomeScreen;
