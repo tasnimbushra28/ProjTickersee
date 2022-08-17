887942a1dd152e41e8de69b46d6fbefb52cd8cc2
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-18 01:01:20 +0600

    screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import data from './data';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import data from '../data';
 
 function App() {
   return (
@@ -13,25 +13,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
-          <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
         </main>
       </div>
     </BrowserRouter>
