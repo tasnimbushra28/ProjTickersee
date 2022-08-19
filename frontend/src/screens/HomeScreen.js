929ab3cb5e591cdbf391d460e64e7f0de80e6b17
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-19 15:18:23 +0600

    frontend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,28 +1,4 @@
-import data from '../data';
-
 function HomeScreen() {
-  return (
-    <div>
-      <h1>Featured Products</h1>
-      <div className="products">
-        {data.products.map((product) => (
-          <div className="product" key={product.slug}>
-            <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </a>
-            <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </a>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add to cart</button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <div>Home Screen</div>;
 }
 export default HomeScreen;
