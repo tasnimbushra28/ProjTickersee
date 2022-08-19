bb01e23cfe5de59042e6fc88f3f6193b9f03fb4c
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-20 00:18:57 +0600

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <Link to="/">tickersee</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>tickersee</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Routes>
