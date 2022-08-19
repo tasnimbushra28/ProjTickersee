07e992771705b9487b72544e432d31b06b974073
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-19 15:00:11 +0600

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,34 +2,32 @@ import data from './data';
 
 function App() {
   return (
-    <BroswerRouter>
-      <div>
-        <header>
-          <a href="/">tickersee</a>
-        </header>
-        <main>
-          <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
+    <div>
+      <header>
+        <a href="/">tickersee</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
                 <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
+                  <p>{product.name}</p>
                 </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
               </div>
-            ))}
-          </div>
-        </main>
-      </div>
-    </BroswerRouter>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
   );
 }
 
