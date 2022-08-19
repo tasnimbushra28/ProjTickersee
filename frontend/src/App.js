b4d1d6db28a5346f83e6fcb108e15138784ba902
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-19 15:21:02 +0600

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,25 +13,6 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
-          <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info">
-                  <a href={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </a>
-                  <p>
-                    <strong>${product.price}</strong>
-                  </p>
-                  <button>Add to cart</button>
-                </div>
-              </div>
-            ))}
-          </div>
         </main>
       </div>
     </BrowserRouter>
