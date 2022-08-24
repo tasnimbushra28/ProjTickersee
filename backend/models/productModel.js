189a709ddcb400e34ffe4386405c8282c69ac787
Tasnim Bushra <tasnim.bushra@g.bracu.ac.bd>
2022-08-24 18:27:20 +0600

    reviews

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -10,6 +10,7 @@ const productSchema = new mongoose.Schema(
     description: { type: String, required: true },
     price: { type: Number, required: true },
     countInStock: { type: Number, required: true },
+    reviews: [reviewSchema],
     rating: { type: Number, required: true },
     numReviews: { type: Number, required: true },
   },
